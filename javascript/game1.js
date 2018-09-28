6a8d03a014e0f9053e10e1f256322c8f44092aa7
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2018-09-27 22:24:33 -0600

    text comes up

--- javascript/game1.js
+++ javascript/game1.js
@@ -2,32 +2,41 @@
 Jeffrey Dahmer, Ed Gein, John Wayne Gacy, Ted Bundy
 Point and click on them to talk to them have arrow image to click on to go to a different location*/
 
-
 $('.pic').append('<img src = "main-character.png"></img>')
-
-
 // startGame();
-
 $('button').on('click', () => {
     startgame();
     const $nameInput = $('input').val();
     $('.title').append($nameInput);
     $('input').remove();
-    $('button').remove();
+    $('button').remove(); 
+    $(".expose").text("Jadwiega meets you outside your precinct to discuss her sister's murder");
   }
 );
-
+const fram = "fart"
 const startgame =() => {
     // $('.startgame').empty()
-    console.log(name);
-    
-    $('.pic').on('click', (e) => {
-        $(e.currentTarget)
+    console.log(fram);
+    $('.pic').on('click' , () => {
+        $('.dialogue').text("Thank you for meeting me, detective.  Someone killed my sister and I need your help finding out which one of my neighbors did it!")
+    })
+}
 
-        $('.col-4').innerHTML(`${name}`)
-      }
-    );}
+// const talking = (event) =>{
+
+    //when click on picture dialogue appears in dialogue bos
+    //$(event.currentTarget).off('click').on()
+// }
     
+    // $('.pic').on('click', (e) => {
+    //     $(e.currentTarget)
+    //     // $('.pic').click(function( event ) {
+    //     //     alert( event.currentTarget ); // true
+    //     //   });
+    //   }
+    // )}
+    
+
 // neighbors = [
 //     {
 //         name: "Jeffey Dahmer",
