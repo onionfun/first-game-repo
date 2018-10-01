4f694d5635965161249c9cc948c8e607302d39a5
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2018-10-01 15:32:03 -0600

    added more dialogue

--- javascript/game1.js
+++ javascript/game1.js
@@ -5,22 +5,44 @@
 // //GLOBAL VARS
 // //=======================================================
 // let sceneIndex = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5', 'option6'];
-const dialogue = ['Thank you for meeting me, detective.  Someone killed my sister and I need your help finding out which one of my neighbors did it!',
+const dialogue = ['0 Thank you for meeting me, detective.  Someone killed my sister and I need your help finding out which one of my neighbors did it!',
  "My first neighbor, Jeff, is from WI, and usually hangs out with boys only, but I saw my sister at his house not long before she died.", 
  "My second neighbor, John, is from IL, and is a community leader, but he likes clowns too much and can't be trusted.", 
  "My third neighbor, Ed, is also from WI, and mostly keeps to himself, working in his crafting shop.", 
- "Jeff: Oh hey, what are you doing here?", 
- 'Hi Jeff, this is the detective helping me with some family stuff...',
- 'Jeff: Nice to meet you.  Is this about your sister? Tragic stuff... welp gotta go!', 
- '~He seemed really strange and apathetic about the death~',
-'dialogue9', 
-'dialogue10', 
-'dialogue11', 
-'dialogue12', 
-'dialogue13', 
-'dialogu14', 
-'dialogue15', 
-'dialogue16'];
+ "JEFF: Jadwiga, what are you doing here?", 
+ "I'm getting help from the detective about my sister.",
+ "JEFF: Sorry to hear that. Tragic stuff, how are you holding up?", 
+ "I'm doing all right, just trying to find answers, can we ask you some questions?",
+ "JEFF: Sure",
+ "Where were you the night of October the 1st?",
+ "JEFF, placing his hand behind his head: Well, much to my embarassment, I'm on parole, you can check with PO Chester about that. As far as where I was... I was at El Potrero night club, you can find me there most nights of the week.",
+ "I'll look into that, thanks",
+ "JEFF: Anytime, if you need any more help, here's my number, feel free to call day or night.",
+ "Will do.",
+ "He seemed open and accomodating about the investigation, wouldn't you say?",
+'Click Jadwiga after you have decided: ', 
+"Ok. Why don't I tell you about John.", 
+"JOHN: Did someone say my name?", 
+"Whoa, John, you scared me! What are you doing out here?",
+"JOHN: I was just at the Fundraiser at the children's hospital.  I was dressed as Pogo the Clown!  Making my way to bus station now.",
+"What a great coincidence meeting you here, we just wanted to ask you some questions about October 1st.",
+"JOHN: Sure thing, what would you like to know?",
+"Well, firstly, where were you that night?",
+"JOHN: I was over at the civic center helping out with my local Jaycees, I'm there every Monday night, you can ask Mr. Voorhees.",
+"Ok, what did you guys do afterwards?",
+"JOHN: Afterwards? Well it didn't end until late that night and by then we were all tired, so I headed home to sleep.",
+"All right, we'll check that out, where can I reach you in the meantime?",
+"JOHN: Here's my phone and address.  If I'm not at one of my restaurants, I'm probably with the Jaycees.",
+"Perfect, I'll check on that.", 
+"John always helps out the community, I don't think they could hurt a fly!",
+"Click Jadwiga after you've decided: ",
+"All right, let me check on these alibis and I'll get back with you tomorrow, Jadwiga.", 
+"Ok, I'll meet you here tomorrow!",
+"Jeff looks clean, he's under a lot of scrutiny being on parole, and John's alibi checked out between the Jaycees and his partner.  That leaves one last suspect: Ed.",
+" ~ 12 hours later ~ ",
+"Detective!  Should we find Ed today?", 
+"Yes, I've call him in, should be here any minute now.", 
+"ED: Heidi Ho neighbor!"];
 let stage = 0;
 onClick=() =>{
     if($('.pic').on('click')){
@@ -39,8 +61,26 @@ let charisma = 0;
 let repulsion = 0;
 let progress = 0;
 const $nameInput = $('input').val();
+$('body').on('click',  '.jeff', '.john', ()=>{ 
+    repulsion+=6;
+    document.getElementById("repulsion").innerHTML = repulsion;
+
+});
+
+$('body').on('click',  '.no', ()=>{ 
+    repulsion+=1;
+    document.getElementById("repulsion").innerHTML = repulsion;
+
+});
+$('body').on('click',  '.yes', ()=>{ 
+    charisma+=1;
+    document.getElementById("charisma").innerHTML = charisma;
+
+});
 // ////=======================================================
+// let body = $('body').append('<img src="https://i.gifer.com/QACY.gif"></img>');
 $('.pic').append('<img src = "main-character.png"></img>')
+
 // startGame();
 $('button').on('click', () => {
     events.startgame();
@@ -64,10 +104,6 @@ startgame: () => {
         console.log(stage)
         onClick();
     })
-     
-    // charisma = charisma +1
-    // $('.charisma').html(`${charisma}`)    ^^^^^^STILL USING^^^^^^^^^
-    //.attr('src', 'image source')
     console.log(fram);
 },
 
@@ -76,22 +112,56 @@ advanceDialogue: ()=>{
     $('.pic').on('click' , () => {
         $('.dialogue').text(`${dialogue[0]}`);
         if(stage ===5){
-            $("#killer").append('<img src = "https://i.pinimg.com/originals/e2/c3/a2/e2c3a29e1d33e50e5a0df33e3d650792.png"></img>')
+            $("#killer").append('<img src = "https://i.pinimg.com/originals/e2/c3/a2/e2c3a29e1d33e50e5a0df33e3d650792.png"></img>');
         }
-        if(stage===8){
-            $("#killer").remove('<img src = "https://i.pinimg.com/originals/e2/c3/a2/e2c3a29e1d33e50e5a0df33e3d650792.png"></img>')
+        if(stage===14){
+            $("#killer").empty();
         }
-    })
+        if(stage===15){
+            $('.dialogue').append('<button class="yes btn-primary">I agree</button>');
+            $('.dialogue').append('<button class="no btn-success">I disagree</button>');
+        }
+        if(stage===18){
+            $("#killer").append('<img src = "Images/john-wayne.png"></img>');
+        }
+        if(stage===28){
+            $('#killer').empty();
+        }
+        if(stage ===30){
+            $('.dialogue').append('<button class="yes btn-primary">I agree</button>');
+            $('.dialogue').append('<button class="no btn-success">I disagree</button>');
+        }
+        if(stage===37){
+            $("#killer").append('<img src = "Images/ed-gein.png"></img>');
 
+
+
+
+
+
+
+
+
+        if(repulsion >= 3){
+        events.gameover();
+    }
+    
+    })
 },
 
 endgame: ()=>{
-        if(repulsion === 6){
-            $('.body').append('<img src = "https://www.pixelcrumb.com/wp-content/uploads/2017/05/GAME-OVER-MAN-still.jpg"></img>')
-    window.alert("YOU DIED");
-    $('.container-fluid').empty()
+    $('body').empty()
+    $('body').css('background-image', 'url("https://i.ytimg.com/vi/GzyrLdSS3WY/maxresdefault.jpg")');
+    $('body').append('<h1 class="end">Ed was the killer along, good job</h1>')
+
+},
+
+gameover: ()=>{
+    $('body').empty()
+    $('body').css('background-image', 'url("https://i.ytimg.com/vi/GzyrLdSS3WY/maxresdefault.jpg")');
+    $('body').append('<h1 class="end">YOU were too repulsive and Jadwiga killed you</h1>')
+
         }
-}
 };
 
 
