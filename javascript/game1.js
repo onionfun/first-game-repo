55dec31250434052edb88a78ec794e65df9cb206
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2018-09-30 19:01:06 -0600

    mutli images

--- javascript/game1.js
+++ javascript/game1.js
@@ -2,11 +2,17 @@
 Jeffrey Dahmer, Ed Gein, John Wayne Gacy, Ted Bundy
 Point and click on them to talk to them have arrow image to click on to go to a different location*/
 
+//GLOBAL VARS
+//=======================================================
+let charisma = 0;
+let repulsion = 0;
+let progress = 0;
+const $nameInput = $('input').val();
+////=======================================================
 $('.pic').append('<img src = "main-character.png"></img>')
 // startGame();
 $('button').on('click', () => {
     startgame();
-    const $nameInput = $('input').val();
     $('.title').append($nameInput);
     $('input').remove();
     $('button').remove(); 
@@ -18,29 +24,38 @@ $('button').on('click', () => {
 
 const fram = "junk"
 const startgame =() => {
-    // $('.startgame').empty()
+    // charisma = charisma +1
+    // $('.charisma').html(`${charisma}`)    ^^^^^^STILL USING^^^^^^^^^
+    
     //.attr('src', 'image source')
     console.log(fram);
     $('.pic').on('click' , () => {
         $('.dialogue').text("Thank you for meeting me, detective.  Someone killed my sister and I need your help finding out which one of my neighbors did it!   Click")
-        dialogueOne()
-    
-    });
+        dialogueOne();
+    })
+};
 const dialogueOne =()=>{
+    $('.startgame').empty()
     $('.dialogue').on('click' , () => {
         $('.dialogue').text("My first neighbor, Jeff, is from WI, and usually hangs out with boys only, but I saw my sister at his house not long before she died.")
     dialogueTwo()
-    })}
+    })
+}
 const dialogueTwo=()=>{
     $('.dialogue').on('click' , () => {
         $('.dialogue').text("My other neighbor, John, is from IL, and is a community leader, but he likes clowns too much and can't be trusted.")
     dialogueThree()
-    })}
+    })
+}
 const dialogueThree=()=>{
+    repulsion++;
+    $('.repulsion').html(`${repulsion}`)
     $('.dialogue').on('click' , () => {
         $('.dialogue').text("My neighbor across the way, Ed, is also from WI, and mostly keeps to himself, working in his crafting shop.")
     dialogueFour()
-})}
+})
+
+}
 const dialogueFour=()=>{
     $('.dialogue').on('click' , () => {
         $('.dialogue').text("And finally my neighbor Ted, from VT.  I think he's a cop and he's really nice but maybe a little too nice.  Click Jadwiga ->")
@@ -51,17 +66,38 @@ const meetJeff=()=>{
         $('.pic').on('click' , () => {
         $('.dialogue').text("Oh look, here's Jeff now!")
         $('#killer').append('<img src = "https://i.pinimg.com/originals/e2/c3/a2/e2c3a29e1d33e50e5a0df33e3d650792.png"></img>')
-    $("#killer").on('click',()=>{ 
-        $('.dialogue').append("Oh hey, what are you doing here?")
-    })
-        
+        jeffTalk()
     });
-    
-
-
 }
 
+const jeffTalk=()=>{
+        $('.dialogue').on('click', ()=>{
+            $('.dialogue').text("Oh hey, what are you doing here?")
+        replyOne()
+    });
+}
+const replyOne=()=>{
+    $('#killer').empty();
+    $('.dialogue').on('click', ()=>{
+        $('.dialogue').text(`Hi Jeff, this is ${$nameInput}, they're helping me out with some family stuff...`)
+        jeffReplyOne()
+        $('.dialogue').empty()
+    });
+}
+const jeffReplyOne=()=>{
+    $("#killer").on('click',()=>{
+        $('.dialogue').text(`Nice to mee you ${$nameInput}.  Is this about your sister? Tragic stuff... welp gotta go!`)
+        suspicionOne()
+    });
+}
 
+const suspicionOne=()=>{
+    $(".pic").on('click',()=>{
+        $('#killer').remove()
+        $(".dialogue").text(`~He seemed really strange and apathetic about the death~`)
+        //CLICK OPTIONS 1 OR 2
+
+});
 }
 
 
