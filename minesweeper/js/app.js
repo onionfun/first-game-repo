5426f181328324d36db97570635523db448db4a6
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2018-10-03 11:38:54 -0600

    adding .data

--- minesweeper/js/app.js
+++ minesweeper/js/app.js
@@ -1,22 +1,4 @@
-for(let y = 1; y < 11; y++){
-    $('.game').append(`<div class='game-column game-column-${y}'></div>`)
-    for(let x = 10; x > 0; x--){
-        const gameSquare = $('<div/>');
-        gameSquare.addClass('square');
-        gameSquare.addClass(`square-${y}-${x}`);
-        $(`.game-column-${y}`).append(gameSquare);
-        //if div has x and y attr you can grab it using .attr
-        gameSquare.attr('x', x);
-        gameSquare.attr('y', y);
-    }
-}
-$('.square').on('click', (e)=>{
-    console.log(e.currentTarget)
-    $(e.currentTarget).css('background-color', 'black')
-
-});
-
-gameBoard =[
+const gameBoard =[
     [],
     [],
     [],
@@ -37,11 +19,62 @@ for (let i = 0; i < gameBoard.length; i++) {
     } else {
         row.push(0);
     }
+    $('.square').data(gameBoard, 1)
 }}
 console.log(gameBoard)
 
+for(let y = 1; y < 11; y++){
+    $('.game').append(`<div class='game-column game-column-${y}'></div>`)
+    for(let x = 10; x > 0; x--){
+        const gameSquare = $('<div/>');
+        gameSquare.addClass('square');
+        gameSquare.addClass(`square-${y}-${x}`);
+        $(`.game-column-${y}`).append(gameSquare);
+        //if div has x and y attr you can grab it using .attr
+        gameSquare.attr('x', y);
+        gameSquare.attr('y', x);
+    }
+}
+
+$('.square').on('click', (e)=>{
+    console.log(e.currentTarget)
+    let userX = $(e.currentTarget).attr('x') //grabbing coordinates when I click square
+    let userY = parseInt($(e.currentTarget).attr('y'))
+    console.log(`user clicked ${userX} and ${userY}`)
+    gameBoard[gameBoard.length - 0 - userY][userX]
+
+
+    if(gameBoard === 1){
+        console.log("ham");
+    }
+    $(e.currentTarget).css('background-color', 'black')
+    console.log(gameBoard)
+//find the appropriate square in the js gameboard, comparing virtual gameboard with what they just clicked, see whether x,y is a 1 or 0
+//x coord can stay the same. x can be the same but y has to start at the bottom gameboard.length-1(if you want fow 0) userY
+});
+
+
+$('button').on('click', () => {
+  let seconds = 99;
+const timePassing = setInterval (()=>{
+    seconds--;
+    if(seconds<=0){
+        alert("Ya burnt");
+    seconds = 99;
+    }
+    $('button').remove();
+console.log(seconds)
+$('.timer').text(`Timer: ${seconds}`)  
+}, 1000)
+}
+);
+
+
+
+
 //userX = gameBoard.length - 1 - userY
 //if row[i] == 1 alert ("mine"
+//when square is clicked con
 
 
 
